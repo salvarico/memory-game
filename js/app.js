72d32fe95d8e6eaa36217c162c8618e1cefe4751
salvarico <salvarico@gmail.com>
2018-10-27 14:49:17 -0500

    add modal, new figures, timer, stars

--- js/app.js
+++ js/app.js
@@ -1,38 +1,200 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// declaring global variables
+let unmatchedCardFacedUp, firstUnmatchedCard, secondUnmatchedCard, matches, starRating, time, started, timer, numOfFigures = 8, moves, cardElement;
+const figureNames = ['fa-css3', 'fa-code', 'fa-bug', 'fa-bitcoin', 'fa-html5', 'fa-linux', 'fa-at', 'fa-git'];
+const limitForOneStar = 25, limitForTwoStars = 15;
+const deck = document.querySelector('.deck');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
 
+// shuffle cards and change the DOM with this new info
+function shuffleCards() {
+  const figureNamesShuffled = shuffle(figureNames.concat(figureNames));
+  const figureElements = document.querySelectorAll('.card>i');
+  figureElements.forEach(
+    function(currentValue, currentIndex, listObj) {
+      currentValue.classList.add(figureNamesShuffled[currentIndex]);
+    });
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function resetVariables() {
+  unmatchedCardFacedUp = false, matches = 0, starRating = 3, time = 0, started = false, moves = 0;
+}
+
+function clearNameOfCards() {
+  const figureElements = document.querySelectorAll('.card>i');
+  figureElements.forEach(
+    function(currentValue, currentIndex, listObj) {
+      currentValue.setAttribute("class", "fa");
+    });
+}
+
+function newGame() {
+  resetVariables();
+  resetStars();
+  resetMoves();
+  clearNameOfCards();
+  shuffleCards();
+  resetTimer();
+  faceAllCardsDown();
+}
+
+function resetMoves() {
+  document.querySelector('.moves').innerHTML = moves;
+}
+
+function faceAllCardsDown() {
+  const cardsElement = document.querySelectorAll('.card');
+  for (const cardElement of cardsElement) {
+    if (cardElement.classList.contains('match')) cardElement.classList.remove('match');
+    if (cardElement.classList.contains('show')) cardElement.classList.remove('open', 'show');
+  }
+}
+
+function resetStars() {
+  let starElement;
+  const starsContainer = document.querySelector('.stars');
+  while (starsContainer.childElementCount < 3) {
+    starElement = starsContainer.children[0];
+    starsContainer.appendChild(starElement.cloneNode(true));
+  }
+}
+
+function timerFunc() {
+  ++time;
+  document.querySelector('.timer').innerHTML = changeTimeFormat(time);
+}
+
+function changeTimeFormat(time) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60);
+  if (seconds < 10) {
+    return `${minutes}:0${seconds}`;
+  }
+  else {
+    return `${minutes}:${seconds}`;
+  }
+}
+
+function startTiming() {
+  started = true;
+  timer = setInterval(timerFunc, 1000);
+}
+
+function resetTimer() {
+  clearTimeout(timer);
+  document.querySelector('.timer').innerHTML = "0:00";
+}
+
+function removeStar() {
+  const starToRemove = document.querySelector('.stars>li');
+  starToRemove.parentNode.removeChild(starToRemove);
+}
+
+function cardAlreadySelected(card) {
+  return card.classList.contains('match') || card.classList.contains('open');
+}
+
+function turnCardFacedUp(card) {
+  card.classList.add('open', 'show');
+}
+
+function getCardName(card) {
+  return card.firstElementChild.className;
+}
+
+function updateAndChangeStarRating() {
+  if (moves === limitForOneStar || moves === limitForTwoStars) {
+    removeStar();
+    starRating--;
+  }
+}
+
+function updateDataAfterNewMatch() {
+  matches++;
+  firstUnmatchedCard.classList.remove('open', 'show');
+  firstUnmatchedCard.classList.add('match');
+  secondUnmatchedCard.classList.remove('open', 'show');
+  secondUnmatchedCard.classList.add('match');
+}
+
+function animateUnmatchedCards() {
+  firstUnmatchedCard.animate({ transform: [ 'scale(1)', 'scale(1.2)', 'scale(1)' ], backgroundColor: ["#2e3d49","#f00"], fontSize: ["0px", "32px"] }, { duration: 1000, iterations: 1 });
+  secondUnmatchedCard.animate({ transform: [ 'scale(1)', 'scale(1.2)', 'scale(1)' ], backgroundColor: ["#2e3d49","#f00"], fontSize: ["0px", "32px"] }, { duration: 1000, iterations: 1 });
+}
+
+function sameFigure(firstUnmatchedCard, secondUnmatchedCard) {
+  const nameOfUnmatchedCards = [getCardName(firstUnmatchedCard), getCardName(secondUnmatchedCard)];
+  return nameOfUnmatchedCards[0] == nameOfUnmatchedCards[1];
+}
+
+function cardClicked(event) {
+  if (!started) {
+    startTiming();
+  }
+  cardElement = event.target.closest('.card');
+  if (!cardElement || cardAlreadySelected(cardElement)) {
+    return;
+  }
+  if (!unmatchedCardFacedUp) {
+    firstUnmatchedCard = cardElement;
+    turnCardFacedUp(firstUnmatchedCard);
+    unmatchedCardFacedUp = true;
+  } else {
+    secondUnmatchedCard = cardElement;
+    document.querySelector('.moves').innerHTML = ++moves;
+    turnCardFacedUp(secondUnmatchedCard);
+    updateAndChangeStarRating();
+    if (sameFigure(firstUnmatchedCard, secondUnmatchedCard)) {
+      updateDataAfterNewMatch();
+      if (matches == numOfFigures) {
+        refreshModalData();
+        toggleModal();
+      }
+    }
+    else {
+      animateUnmatchedCards();
+      firstUnmatchedCard.classList.remove('open', 'show');
+      secondUnmatchedCard.classList.remove('open', 'show');
+    }
+    unmatchedCardFacedUp = false;
+  }
+}
+
+function refreshModalData() {
+  const modalTime = document.querySelector('.modal_time');
+  modalTime.innerHTML = `Time = ${changeTimeFormat(time)}`;
+  const modalStars = document.querySelector('.modal_stars');
+  modalStars.innerHTML = `Stars = ${starRating}`;
+  const modalMoves = document.querySelector('.modal_moves');
+  modalMoves.innerHTML = `Moves = ${moves}`;
+}
+
+function startGame() {
+  let restartElement;
+  deck.addEventListener('click', cardClicked);
+  restartElement = document.querySelector('.restart');
+  restartElement.addEventListener('click', newGame);
+  replayButton = document.querySelector('.modal_replay');
+  replayButton.addEventListener('click', () => {toggleModal(); newGame();});
+  cancelButton = document.querySelector('.modal_cancel');
+  cancelButton.addEventListener('click', () => {clearTimeout(timer); toggleModal();});
+  newGame();
+}
+
+function toggleModal() {
+  const modal = document.querySelector('.modal_background');
+  modal.classList.toggle('hide');
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
